8f15502220e21593c67c6f98c7d83de7c4e479bd
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-06-16 11:25:17 +0300

    Modify

--- src/features/vaccine/Vaccine.js
+++ src/features/vaccine/Vaccine.js
@@ -1,40 +1,43 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
-import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectVaccineById } from './vaccinesApiSlice'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectVaccineById } from './vaccinesApiSlice';
+import { TableRow, TableCell, Button, IconButton } from '@mui/material';
 
 // React component representing a vaccine row in a table
 const Vaccine = ({ vaccineId }) => {
-    const vaccine = useSelector(state => selectVaccineById(state, vaccineId)) // Select vaccine from Redux store by vaccineId
-    const navigate = useNavigate() // Hook to navigate to different routes
+    const vaccine = useSelector(state => selectVaccineById(state, vaccineId)); // Select vaccine from Redux store by vaccineId
+    const navigate = useNavigate(); // Hook to navigate to different routes
+    
     if (vaccine) { // If vaccine exists, render vaccine details
         // Function to handle edit button click, navigates to vaccine edit page
-        const handleEdit = () => navigate(`/dash/vaccines/${vaccineId}`)
-        const vaccineDate = new Date(vaccine.date).toLocaleString('en-IL', { day: 'numeric', month: 'long', year: 'numeric' })
-        // const clientId = new String(vaccine.client)
+        const handleEdit = () => navigate(`/dash/vaccines/${vaccineId}`);
+        const vaccineDate = new Date(vaccine.date).toLocaleString('en-IL', { day: 'numeric', month: 'long', year: 'numeric' });
+
         return ( // Render vaccine row
-            <tr className="table__row vaccine">
-                <td className="table__cell accineDate">{vaccineDate}</td>
-                <td className={`table__cell `}>{vaccine.name} </td>
-                <td className={`table__cell `}>
-                    <li>{vaccine.clientName} {vaccine.clientLastName} </li> 
-                    <li> id: {vaccine.clientID} </li>
-                </td>
-                <td className={`table__cell `}>
+            <TableRow>
+                <TableCell>{vaccineDate}</TableCell>
+                <TableCell>{vaccine.name}</TableCell>
+                <TableCell>
+                    <li>{vaccine.clientName} {vaccine.clientLastName}</li>
+                    <li>id: {vaccine.clientID}</li>
+                </TableCell>
+                <TableCell>
                     {/* Edit button */}
-                    <button
-                        className="icon-button table__button"
+                    <IconButton
+                        color="primary"
                         onClick={handleEdit}
                     >
                         {/* Edit icon */}
                         <FontAwesomeIcon icon={faPenToSquare} />
-                    </button>
-                </td>
-            </tr>
-        )
-
-    } else return null  // If vaccine does not exist, return null (no rendering)
+                    </IconButton>
+                </TableCell>
+            </TableRow>
+        );
+    } else {
+        return null;  // If vaccine does not exist, return null (no rendering)
+    }
 };
 
 export default Vaccine;
--- src/features/vaccine/VaccinesList.js
+++ src/features/vaccine/VaccinesList.js
@@ -1,5 +1,6 @@
-import { useGetVaccinesQuery } from "./vaccinesApiSlice"
+import { useGetVaccinesQuery } from "./vaccinesApiSlice";
 import Vaccine from "./Vaccine"; // Import the Vaccine component
+import { Table, TableHead, TableBody, TableRow, TableCell, Typography, CircularProgress } from '@mui/material';
 
 const VaccinesList = () => {
     const {
@@ -8,43 +9,42 @@ const VaccinesList = () => {
         isSuccess,
         isError,
         error  
-     } = useGetVaccinesQuery(
-        'VaccinesList', {
-            pollingInterval: 60000,
-            refetchOnFocus: true,
-            refetchOnMountOrArgChange: true
-        });
-     
+     } = useGetVaccinesQuery('VaccinesList', {
+        pollingInterval: 60000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    });
  
-     let content // Define content variable to render based on loading and success states     
-     if(isLoading) content = <p>Louding vaccines...</p>// Display loading message while fetching data
-     // Display error message if fetching data results in an error
-     if(isError) content = <p className="errmsg">{error?.data?.message}</p>
-     if (isSuccess) { // If data fetching is successful, render the vaccines list table
-         const { ids } = vaccines // Generate table rows for each vaccine
-         const tableContent = ids?.length // It has to have a key
-             ? ids.map(vaccineId => <Vaccine key={vaccineId} vaccineId={vaccineId} />)
-             : null
- 
-         content = ( // Render the vaccines list table
-            
-             <table className="tableV table--vaccines">
-                 <thead className="table__thead">
-                     <tr>
-                         <th scope="col" className="table__th vaccine__vaccinename">Vaccine Date</th>
-                         <th scope="col" className="table__th vaccine__roles">Vaccine Name</th>
-                         <th scope="col" className="table__th vaccine__edit">Client INFO</th>
-                         <th scope="col" className="table__th vaccine__edit">Edit</th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                     {tableContent}
-                 </tbody>
-             </table>
-             
-            
-         )
-     }
-    return content // Render the content based on the current state
+    let content; // Define content variable to render based on loading and success states     
+
+    if (isLoading) content = <CircularProgress />; // Display loading spinner while fetching data
+
+    if (isError) content = <Typography color="error">{error?.data?.message}</Typography>; // Display error message if fetching data results in an error
+
+    if (isSuccess) { // If data fetching is successful, render the vaccines list table
+        const { ids } = vaccines; // Generate table rows for each vaccine
+        const tableContent = ids?.length // It has to have a key
+            ? ids.map(vaccineId => <Vaccine key={vaccineId} vaccineId={vaccineId} />)
+            : null;
+
+        content = ( // Render the vaccines list table
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Vaccine Date</TableCell>
+                        <TableCell>Vaccine Name</TableCell>
+                        <TableCell>Client INFO</TableCell>
+                        <TableCell>Edit</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tableContent}
+                </TableBody>
+            </Table>
+        );
+    }
+
+    return content; // Render the content based on the current state
 }
-export default VaccinesList
+
+export default VaccinesList;
