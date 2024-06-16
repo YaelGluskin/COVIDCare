9ca6fb67870c99dbb97792e56ae4e2e3f8fdf884
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-06-16 11:44:03 +0300

    Revert "Modify"
    This reverts commit 8f15502220e21593c67c6f98c7d83de7c4e479bd.

--- src/features/vaccine/Vaccine.js
+++ src/features/vaccine/Vaccine.js
@@ -1,43 +1,40 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectVaccineById } from './vaccinesApiSlice';
-import { TableRow, TableCell, Button, IconButton } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectVaccineById } from './vaccinesApiSlice'
 
 // React component representing a vaccine row in a table
 const Vaccine = ({ vaccineId }) => {
-    const vaccine = useSelector(state => selectVaccineById(state, vaccineId)); // Select vaccine from Redux store by vaccineId
-    const navigate = useNavigate(); // Hook to navigate to different routes
-    
+    const vaccine = useSelector(state => selectVaccineById(state, vaccineId)) // Select vaccine from Redux store by vaccineId
+    const navigate = useNavigate() // Hook to navigate to different routes
     if (vaccine) { // If vaccine exists, render vaccine details
         // Function to handle edit button click, navigates to vaccine edit page
-        const handleEdit = () => navigate(`/dash/vaccines/${vaccineId}`);
-        const vaccineDate = new Date(vaccine.date).toLocaleString('en-IL', { day: 'numeric', month: 'long', year: 'numeric' });
-
+        const handleEdit = () => navigate(`/dash/vaccines/${vaccineId}`)
+        const vaccineDate = new Date(vaccine.date).toLocaleString('en-IL', { day: 'numeric', month: 'long', year: 'numeric' })
+        // const clientId = new String(vaccine.client)
         return ( // Render vaccine row
-            <TableRow>
-                <TableCell>{vaccineDate}</TableCell>
-                <TableCell>{vaccine.name}</TableCell>
-                <TableCell>
-                    <li>{vaccine.clientName} {vaccine.clientLastName}</li>
-                    <li>id: {vaccine.clientID}</li>
-                </TableCell>
-                <TableCell>
+            <tr className="table__row vaccine">
+                <td className="table__cell accineDate">{vaccineDate}</td>
+                <td className={`table__cell `}>{vaccine.name} </td>
+                <td className={`table__cell `}>
+                    <li>{vaccine.clientName} {vaccine.clientLastName} </li> 
+                    <li> id: {vaccine.clientID} </li>
+                </td>
+                <td className={`table__cell `}>
                     {/* Edit button */}
-                    <IconButton
-                        color="primary"
+                    <button
+                        className="icon-button table__button"
                         onClick={handleEdit}
                     >
                         {/* Edit icon */}
                         <FontAwesomeIcon icon={faPenToSquare} />
-                    </IconButton>
-                </TableCell>
-            </TableRow>
-        );
-    } else {
-        return null;  // If vaccine does not exist, return null (no rendering)
-    }
+                    </button>
+                </td>
+            </tr>
+        )
+
+    } else return null  // If vaccine does not exist, return null (no rendering)
 };
 
 export default Vaccine;
